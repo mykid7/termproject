4c669afcdf7c67d0b5dae3d658e34d3fbc086ab7
mykid7 <55013563+mykid7@users.noreply.github.com>
2019-11-11 00:47:09 +0900

    Update frame.c

--- frame.c
+++ frame.c
@@ -5,14 +5,11 @@ int main(void)
 {
 	int i, j;
 	
-	printf("input size column_row");
-	scanf("%d %d", &i, &j);
-	
 	int frame[9][9] = {0};
 	
 	for (i = 0; i < 9; i++)
 	{
-		if (i % 3 == 0) printf(" ");
+		if (i % 3 == 0) printf("\n");
 		if (i % 9 == 0) printf("\n");
 		for (j = 0; j < 9; j++)
 		{
